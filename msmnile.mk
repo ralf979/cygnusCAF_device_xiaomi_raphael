51b8e9342b8208593eb65e222d096633f8d767ef
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    Revert "raphael: Switch to common basic USB HAL"
    This reverts commit 012e786715533d8dc749e6d98a22bf8450c9586f.

--- msmnile.mk
+++ msmnile.mk
@@ -418,7 +418,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
